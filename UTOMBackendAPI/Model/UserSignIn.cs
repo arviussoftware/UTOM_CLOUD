ab39ca5fb7b84eae965b9b862c8980f1255bfff1
Garun Kumar <garun@arviussoft.com>
2025-12-29 13:13:46 +0530

    Initial Commit

---
+++ UTOMBackendAPI/Model/UserSignIn.cs
@@ -0,0 +1,8 @@
+﻿namespace UTOMBackendAPI.Model
+{
+    public class UserSignIn
+    {
+        public string UserName { get; set; }= string.Empty!;
+        public string UserPassword { get; set; } = string.Empty!;
+    }
+}
