b6b51ef53cde1432f0efd85c46497d32bd4ef540
Garun Chaudhary <keshavsaini079@gmail.com>
2025-12-29 12:23:52 +0530

    Initial commit

---
+++ UTOMBackendAPI/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UTOMBackendAPI.Model;
+
+namespace UTOMBackendAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost("Login")]
+        public IActionResult Login([FromBody] UserSignIn userSignIn)
+        {
+            return Ok(new
+            {
+                Message = "Login successful",
+                userInfo= userSignIn
+            });
+        }
+    }
+}
