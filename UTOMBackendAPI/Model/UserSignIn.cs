7f816b54c22b0a014ad871649a7e38ad7fc875f2
Arvius Software <arviussoftware@gmail.com>
2025-12-29 12:58:47 +0530

    Delete UTOMBackendAPI directory

--- UTOMBackendAPI/Model/UserSignIn.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UTOMBackendAPI.Model
-{
-    public class UserSignIn
-    {
-        public string UserName { get; set; }= string.Empty!;
-        public string UserPassword { get; set; } = string.Empty!;
-    }
-}
